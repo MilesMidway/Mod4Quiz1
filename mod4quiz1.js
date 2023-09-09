cae96cd86d2f9776a2cfea421e5a70c3cceefa2f
Zyd8 <115306019+Zyd8@users.noreply.github.com>
2023-09-09 22:28:17 +0800

    Initial commit

---
+++ mod4quiz1.js
@@ -0,0 +1,39 @@
+class Day
+{
+    _day;
+
+    constructor(day)
+    {
+        this.day = day;
+    }
+
+    set day(value)
+    {
+        this._day = value;
+    }
+
+    get day()
+    {
+        return this._day;
+    }
+
+    printDay()
+    {
+
+    }
+
+    nextDay()
+    {
+
+    }
+
+    previousDay()
+    {
+
+    }
+
+    calculateDay()
+    {
+
+    }
+}
